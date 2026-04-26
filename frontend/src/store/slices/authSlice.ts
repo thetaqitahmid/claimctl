c84289063f1dd533e542b7841c2115f17241dd10
Taqi Tahmid <mdtaqitahmid@gmail.com>
2026-04-26 10:33:06 +0300

    fix(frontend): remove whitespace from authSlice

--- frontend/src/store/slices/authSlice.ts
+++ frontend/src/store/slices/authSlice.ts
@@ -3,14 +3,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 interface CredentialsPayload {
   user: string | null;
   email: string | null;
-
   role: string | null;
 }
 
 const initialState: CredentialsPayload = {
   user: null,
   email: null,
-
   role: null,
 };
 
@@ -22,13 +20,11 @@ const authSlice = createSlice({
       const { user, email, role } = action.payload;
       state.user = user;
       state.email = email;
-
       state.role = role;
     },
     clearCredentials: (state) => {
       state.user = null;
       state.email = null;
-
       state.role = null;
     },
   },
